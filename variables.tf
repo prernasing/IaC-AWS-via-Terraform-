69224b091b2ea4c208115c0fa6bfdf72634777af
prernasing <86356563+prernasing@users.noreply.github.com>
2021-10-18 23:17:21 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "instance_count" {
+    default = 2
+}
+variable "availability_zone" {
+
+}
+variable "access_key" {
+    default = "AKIAQ2QWUYAFVIKYNCHM"
+}
+
+variable "secret_key" {
+    default = "SDErq3Qh4E2PsOC1stWdHz+eSUqG/7dXMVdFvmvW"
+}
+
+variable "ami" {
+    default = "ami-00dfe2c7ce89a450b"
+}
+
+variable "region" {
+    default = "us-east-2"
+}
