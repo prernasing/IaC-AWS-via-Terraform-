e052cc268b3509b96fbd180fca3c1ad3390a3169
prernasing <86356563+prernasing@users.noreply.github.com>
2021-10-18 23:13:10 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,24 @@
+#with variable defined in it
 
+provider "aws" {
+    region = var.region
+    secret_key = var.secret_key
+    access_key = var.access_key
+}
+
+resource "aws_instance" "my_ec2"{
+    ami = var.ami
+    instance_type = var.instance_type
+    count = var.instance_count
+    security_groups = [aws_security_group.my_sg.name]
+    availability_zone = var.availability_zone 
+tags = {
+      env = "dev"
+    }
+}
+resource "aws_default_vpc" "my_vpc" {
+    tags = {
+        vpc = "default"
+    }
+    
+}
